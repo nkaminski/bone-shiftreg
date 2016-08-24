809b7e7f36920b792995806a65d666ebcb087b6c
Nash Kaminski <nashkaminski@gmail.com>
2016-08-24 02:08:33 -0500

    Ready for full test

--- redis-client/main.c
+++ redis-client/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include <assert.h>
 #include <hiredis.h>
 #include "confparse.h"
 #include "io.h"
@@ -22,15 +23,15 @@ int main (int argc, char **argv) {
     return 1;
   }
   /* Open remoteproc device */
-  //if((iofd = io_init(cfg_getstr(cfg,"pru-remoteproc-file"))) < 0){
-  //  fprintf(stderr,"Failed to open remoteproc device file\n");
-  //  return 1;
-  //}
+  if((iofd = io_init(cfg_getstr(cfg,"pru-remoteproc-file"))) < 0){
+    fprintf(stderr,"Failed to open remoteproc device file\n");
+    return 1;
+  }
   /* Set output channel count */
-  //if(io_set_nchannels(iofd, cfg_getint(cfg,"num-channels")) < 0){
-  //  io_close(iofd);
-  //  return 1;
-  //}
+  if(io_set_nchannels(iofd, cfg_getint(cfg,"num-channels")) < 0){
+    io_close(iofd);
+    return 1;
+  }
   /* Establish Redis connection */
   printf("Connecting to redis at %s, port %ld\n", cfg_getstr(cfg,"redis-host"), cfg_getint(cfg,"redis-port"));
   c = redisConnect(cfg_getstr(cfg,"redis-host"), cfg_getint(cfg,"redis-port"));
@@ -57,7 +58,11 @@ int main (int argc, char **argv) {
   }
   }
   /* Message handling loop */
-  printf("subscribed...\n");
+  char *respcpy, *ascval;
+  unsigned int chan, val, start, nchan;
+  start = cfg_getint(cfg,"start-address");
+  nchan = cfg_getint(cfg,"num-channels");
+  printf("Subscribed...\n");
   reply = redisCommand(c, "SUBSCRIBE changes");
   freeReplyObject(reply);
   while(redisGetReply(c,(void *)&reply) == REDIS_OK) {
@@ -65,13 +70,38 @@ int main (int argc, char **argv) {
     // consume message
     if (reply == NULL) 
       continue;
-    printf("reply ok %u \n", reply->type);
     if (reply->type == REDIS_REPLY_ARRAY) {
-      for (int j = 0; j < reply->elements; j++) {
-        printf("%u) %s\n", j, reply->element[j]->str);
+      if(reply->elements == 3) {
+        if(reply->element[0]->type == REDIS_REPLY_STRING && reply->element[2]->type == REDIS_REPLY_STRING && strcmp("message", reply->element[0]->str) == 0){
+          printf("Processing incoming message %s\n", reply->element[2]->str);
+          //Message parsing
+          respcpy = malloc(strlen(reply->element[2]->str)+1);
+          assert(respcpy != NULL);
+          strcpy(respcpy, reply->element[2]->str);
+          ascval = strsep(&respcpy, ":");
+          if(respcpy != NULL){
+            chan = atoi(ascval);
+            val = atoi(respcpy);
+            //Does this channel belong to us? If so, update!
+            if(chan >= start && chan < (start + nchan)){
+              printf("COMMAND: channel %d -> %d\n", chan, val);
+              printf("IO Command: address %d -> %d\n", chan - start, val);
+              //TODO Error handling and || output
+              io_set_pwm(iofd, 0, chan-start, val);
+            } else {
+              printf("Channel %d is not on our board, ignoring.\n", chan);
+            }
+          } else {
+            fprintf(stderr, "Malformed message published to channel!\n");
+          }
+          free(ascval);
+        }
+      } else {
+        fprintf(stderr, "Invalid number of elements in reply array!\n");
       }
     }
     freeReplyObject(reply);
   }
+  io_close(iofd);
   cfg_free(cfg);
 }
